877960807a441a1ce7282e51f4b9ef782bd4fa5b
ajrominger <ajrominger@gmail.com>
2018-01-18 16:27:37 -0700

    finished

--- toyOutput/sad_logLikZ.py
+++ toyOutput/sad_logLikZ.py
@@ -8,23 +8,30 @@ def sad_logLikZ(x):
        of the observed data given the model and the likelihood of an ideal dataset"""
     x = x[x > 0]
     
-    y = fitFisher(x)
+    thisFit = fitFisher(x)
     
-    return y
+    x0 = np.array(range(np.power(10, 6))) + 1
+    p0 = np.log(fisherPMF(x0, thisFit[0]))
+    p0 = p0[np.isfinite(p0)]
+    
+    n = x.size
+    m = sum(p0 * np.exp(p0)) * n
+    v = sum(np.power(m/n - p0, 2) * np.exp(p0)) * n
+    
+    return np.power((thisFit[1] - m) / np.power(v, 0.5), 2)
 
 def betax(z, a, b):
     """betax -- a helper function for fisherCMF that uses hypergeometric for 
        accuracy and speed"""
     return 1/a * np.power(z, a) * np.power(1-z, b) * special.hyp2f1(a+b, 1, a+1, z)
 
-
 def fisherCMF(x, beta):
     """fisherCMF -- the CMF for the fisher logseries"""
     return 1 + betax(np.exp(-beta), x+1, 0) / np.log(1 - np.exp(-beta))
 
 def fisherPMF(x, beta):
+    """fisherPMF -- the PMF for the fisher logseries"""
     return 1 / np.log(1 / (1 - np.exp(-beta))) * np.exp(-beta * x) / x
-
 
 def fitFisher(x):
     """ML for fisher logseries"""
@@ -41,4 +48,6 @@ def fitFisher(x):
     
     return [mle, ll]
 
-sad_logLikZ(np.array([1, 2, 3, 4]))
+
+## example of how it works
+# sad_logLikZ(np.array([1, 2, 3, 4]))
